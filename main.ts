728cca78125df8bd2c5cf624ef8a69e85088ef29
baa <34665120+baa-mb@users.noreply.github.com>
2021-10-04 23:29:18 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    laufrichtung = 1
     pins.servoWritePin(AnalogPin.P16, 0)
     basic.showLeds(`
         . . . . .
@@ -9,6 +10,7 @@ input.onButtonPressed(Button.A, function () {
         `)
 })
 input.onButtonPressed(Button.AB, function () {
+    laufrichtung = 0
     basic.showIcon(IconNames.Diamond)
     pins.servoWritePin(AnalogPin.P16, 90)
 })
@@ -30,13 +32,12 @@ radio.onReceivedString(function (receivedString) {
                 EventBusSource.MICROBIT_ID_BUTTON_B,
                 EventBusValue.MICROBIT_BUTTON_EVT_CLICK
                 )
-            } else {
-                lenkung(parseFloat(receivedString))
             }
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
+    laufrichtung = -1
     pins.servoWritePin(AnalogPin.P16, 180)
     basic.showLeds(`
         . . # . .
@@ -50,7 +51,22 @@ function lenkung (num: number) {
     winkel = Math.map(num, -1023, 1023, 0, 180)
     pins.servoWritePin(AnalogPin.P1, winkel)
 }
+let abstand = 0
 let winkel = 0
+let laufrichtung = 0
 basic.showString("DLPL")
 basic.showIcon(IconNames.Diamond)
 radio.setGroup(54)
+laufrichtung = 0
+basic.forever(function () {
+    abstand = sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    if (abstand < 5) {
+        laufrichtung = 0
+        pins.servoWritePin(AnalogPin.P16, 90)
+    }
+    basic.pause(1000)
+})
